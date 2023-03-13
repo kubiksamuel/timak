aa47aeac6e7cbbaffb3c73119e2f1b5d5ce008a5
simonkrupa <simon.krupa@icloud.com>
2023-03-13 16:21:45 +0100

    index page func

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,15 +4,14 @@
         <button v-if="!account" class="bg-green-300 rounded p-4" @click="connectWallet">Connect Wallet</button>
         <div v-if="account" class="mt-5">
             <input v-model="messageInput" name="guestBookInfo" class="py-4 px-4 shadow border rounded" maxlength="20" />
-            <button class="bg-yellow-300 rounded p-4 mt-10" @click="wave(messageInput)">Send</button>
+            <button class="bg-yellow-300 rounded p-4 mt-10" @click="createRepository(messageInput)">Send</button>
         </div>
 
         <div v-if="account" class="border shadow w-4/12 p-4 mt-10">
-            <h3 class="text-2xl">Number Of Entries: {{ guestPostsCount }}</h3>
-            <div v-for="(guestPost, idx) in guestPosts" :key="idx" class="flex flex-col m-auto" :class="{ 'mt-4': idx > 1 }">
-                <div v-if="guestPost.message" class="flex justify-between w-full">
-                    <span class="font-semibold">{{ guestPost.timestamp }}</span>
-                    <span>{{ guestPost.message }}</span>
+            <div v-for="(postedRepository, idx) in postedRepositories" :key="idx" class="flex flex-col m-auto" :class="{ 'mt-4': idx > 1 }">
+                <div v-if="postedRepository.message" class="flex justify-between w-full">
+                    <span class="font-semibold">{{ postedRepository.timestamp }}</span>
+                    <span>{{ postedRepository.message }}</span>
                 </div>
             </div>
         </div>
@@ -25,14 +24,15 @@ meta:
 <script setup lang="ts">
 import { ref } from "vue"
 import { storeToRefs } from "pinia"
-import { useCryptoStore } from "~/stores/crypto"
+import { useRepositoryStore } from "~/stores/repos"
 const something = "s"
 console.log("something: ", something)
 if (something == "s") {
     console.log(something)
 }
 const messageInput = ref(null as any)
-const cryptoStore = useCryptoStore()
-const { wave, connectWallet } = useCryptoStore()
-const { account, guestPosts, guestPostsCount } = storeToRefs(cryptoStore)
+const repositoryStore = useRepositoryStore()
+const { createRepository, connectWallet } = useRepositoryStore()
+const { postedRepositories, account } = storeToRefs(repositoryStore)
+console.log(postedRepositories)
 </script>
