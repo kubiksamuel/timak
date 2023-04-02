b8d0126db49e488d210da94b79488e6d7aeca365
MartinSiran <matosiro4@gmail.com>
2023-04-02 22:28:48 +0200

    feat: download file from ipfs

--- src/composables/ipfs.ts
+++ src/composables/ipfs.ts
@@ -1,23 +1,36 @@
 import axios from 'axios'
 
-const api_endpoint_pinata_post = "https://api.pinata.cloud/pinning/"
-const api_endpoint_pinata_get = "https://gateway.pinata.cloud/ipfs/pinFileToIPFS"
+const api_endpoint_pinata_post = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+const api_endpoint_pinata_get = "https://gateway.pinata.cloud/ipfs/"
 const access_token = "..."
 const authorization = "Bearer " + access_token
-export const getFromIPFS = async (fileHash: string) => {
+
+export const getFromIPFS = async (fileHash: string, fileName: string) => {
     const res = await axios.get(api_endpoint_pinata_get + fileHash, {
         headers: {
-            'Accept': 'text/plain'
-        }
+            'Accept': 'text/plain',
+        },
+        responseType: 'blob'
     })
     console.log(res)
+    const href = URL.createObjectURL(res.data);
+
+    const link = document.createElement('a');
+    link.href = href;
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+    return res.data
 }
 
-export const addToIPFS = async (fileToUpload: Blob) => {
+export const addToIPFS = async (fileToUpload: Blob, fileName: string) => {
     const formData = new FormData()
     formData.append("file", fileToUpload)
     formData.append("pinataMetadata", JSON.stringify({
-        name: "test"
+        name: fileName
     }))
     formData.append("pinataOptions", JSON.stringify({
         cidVersion: 0
@@ -29,5 +42,5 @@ export const addToIPFS = async (fileToUpload: Blob) => {
             "Authorization": authorization
         }
     })
-    console.log(res.data)
+    return res.data.IpfsHash
 }
