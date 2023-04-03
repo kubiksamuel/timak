819cb9066ff78426adcab0771f6ed6ad6033806e
MartinSiran <matosiro4@gmail.com>
2023-04-03 16:02:30 +0200

    feat: upload whole version (folder)

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    AddFileSlideOver: typeof import('./components/AddFileSlideOver.vue')['default']
+    AddVersionSlideOver: typeof import('./components/AddVersionSlideOver.vue')['default']
     CreateRepositorySlideOver: typeof import('./components/CreateRepositorySlideOver.vue')['default']
     Loader: typeof import('./components/Loader.vue')['default']
     README: typeof import('./components/README.md')['default']
