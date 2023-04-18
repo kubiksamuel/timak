6dc31952eb95473be042bf7d168285e6ed7c5e20
Samuel Kubík <samuel.kubik@pixwell.sk>
2023-04-18 13:13:46 +0200

    fix: delete redundant current route

--- src/layouts/SidebarLayout.vue
+++ src/layouts/SidebarLayout.vue
@@ -22,16 +22,9 @@
                                 <li v-for="item in navigation" :key="item.name">
                                     <button
                                         @click="redirect(item.href)"
-                                        :class="[
-                                            item.href === currentRoute ? 'bg-violet-700 text-white' : 'text-violet-200 hover:text-white hover:bg-violet-700',
-                                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full',
-                                        ]"
+                                        class="text-violet-200 hover:text-white hover:bg-violet-700 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full"
                                     >
-                                        <component
-                                            :is="item.icon"
-                                            :class="[item.href === currentRoute ? 'text-white' : 'text-violet-200 group-hover:text-white', 'h-6 w-6 shrink-0']"
-                                            aria-hidden="true"
-                                        />
+                                        <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
                                         {{ item.name }}
                                     </button>
                                 </li>
