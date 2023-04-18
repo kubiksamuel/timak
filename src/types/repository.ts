04e9ca149650e3b238dfb81754a49f211b4ac1d2
Samuel Kubík <samuel.kubik@pixwell.sk>
2023-04-18 13:13:03 +0200

    add: review type

--- src/types/repository.ts
+++ src/types/repository.ts
@@ -2,3 +2,12 @@ export type RepositoryMeta = {
     name: string
     description: string
 }
+
+export type Review = {
+    repositoryHash: string
+    reviewerAddress: string
+    reviewerSkillLevel: number
+    rating: number
+    ipfsHash: string
+    // versionId: string
+}
