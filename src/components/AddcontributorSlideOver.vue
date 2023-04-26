bc06dfc765284fcc4c544b656f5e6840a848c337
Samuel Kubík <samuel.kubik@pixwell.sk>
2023-04-26 19:37:08 +0200

    add: slideover footer

--- src/components/AddcontributorSlideOver.vue
+++ src/components/AddcontributorSlideOver.vue
@@ -1,32 +1,37 @@
 <template>
     <SlideOver v-bind="{ open }" @close="emit('close')" :title="title">
-        <div class="flex flex-col space-y-2">
-            <div>
-                <label for="address" class="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900">Address</label>
-                <div class="mt-1">
-                    <input
-                        v-model="newRepository.address"
-                        type="text"
-                        name="address"
-                        id="address"
-                        class="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Address"
-                    />
+        <div class="flex h-full flex-col pt-1">
+            <div class="h-full flex-1">
+                <div class="flex flex-col space-y-3">
+                    <div>
+                        <label for="address" class="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900">Address</label>
+                        <div class="mt-1">
+                            <input
+                                v-model="newRepository.address"
+                                type="text"
+                                name="address"
+                                id="address"
+                                class="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Address"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label for="address" class="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900">Name of contributor</label>
+                        <div class="mt-1">
+                            <input
+                                v-model="newRepository.nameOfContributor"
+                                type="text"
+                                name="address"
+                                id="address"
+                                class="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Name"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div>
-                <label for="address" class="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900">Name of contributor</label>
-                <div class="mt-1">
-                    <input
-                        v-model="newRepository.nameOfContributor"
-                        type="text"
-                        name="address"
-                        id="address"
-                        class="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Name"
-                    />
-                </div>
-            </div>
+            <hr class="-mx-6" />
 
             <div class="px-4 py-3 text-right sm:px-6">
                 <button
@@ -45,7 +50,7 @@ import { ref, Ref } from "vue"
 import SlideOver from "./SlideOver.vue"
 import { useRepositoryStore } from "~/stores/repos"
 import { RepositoryMeta } from "~/types/repository"
-import { storeToRefs } from "pinia";
+import { storeToRefs } from "pinia"
 
 const { createRepository, addContributor } = useRepositoryStore()
 
